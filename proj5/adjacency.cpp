494e9b798edb560536899c8e762f333cdaa070b9
Benjamin Zachary Webber <gqb217@hydra18.eecs.utk.edu>
2020-04-15 17:32:35 -0400

    Eliminated warnings

--- proj5/adjacency.cpp
+++ proj5/adjacency.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 std::vector< std::vector<int> > get_edges(std::vector<std::string> dice, std::vector<std::string> words) {
 	std::vector< std::vector<int> > dice_edges;
 	std::vector<int> dice_word_edge_buff;
@@ -17,10 +19,10 @@ std::vector< std::vector<int> > get_edges(std::vector<std::string> dice, std::ve
 	//Dice is on the outermost level so the amount of edges added to the dice_edges
 	// vector is the same as the number of dice.
 
-	for (int i = 0; i < dice.size(); i++) {
-		for (int j = 0; j < words.size(); j++) {
-			for (int k = 0; k < words[j].size(); k++) {
-				for (int l = 0; l < dice[i].size(); l++) {
+	for (int i = 0; i < (int)dice.size(); i++) {
+		for (int j = 0; j < (int)words.size(); j++) {
+			for (int k = 0; k < (int)words[j].size(); k++) {
+				for (int l = 0; l < (int)dice[i].size(); l++) {
 					if (dice[i][l] == words[j][k]) {
 						dice_word_conn = true;
 						break;
@@ -57,7 +59,7 @@ int main(int argc, char* argv[]) {
 	std::vector< std::vector<int> > dice_edges;
 
 	if (argc != 3) {
-		std::cout << "Usage: " << argv[0] << " [Dice file] [Word file]";
+		std::cout << "Usage: " << argv[0] << " [Dice file] [Word file]\n";
 		return 1;
 	}
 
@@ -65,17 +67,17 @@ int main(int argc, char* argv[]) {
 	words = get_lines(argv[2]);
 
 	std::cout << "Dice:" << std::endl;
-	for (int i = 0; i < dice.size(); i++)
+	for (int i = 0; i < (int)dice.size(); i++)
 		std::cout << '\t' << i << ' ' << dice[i] << std::endl;
 	std::cout << "Words:" << std::endl;
-	for (int i = 0; i < words.size(); i++)
+	for (int i = 0; i < (int)words.size(); i++)
 		std::cout << '\t' << i << ' ' << words[i] << std::endl;
 
 	dice_edges = get_edges(dice, words);
 
-	for (int i = 0; i < dice_edges.size(); i++) {
+	for (int i = 0; i < (int)dice_edges.size(); i++) {
 		std::cout << "Dice " << i << " (" << dice[i] <<')' << std::endl;
-		for (int j = 0; j < dice_edges[i].size(); j++)
+		for (int j = 0; j < (int)dice_edges[i].size(); j++)
 			std::cout << '\t' << dice_edges[i][j] << " (" << words[dice_edges[i][j]] << ')' << std::endl;
 	}
 }
